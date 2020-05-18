3c3a7f5d8c050c1743fdca985809f01cbd77232a
Wei Lee <weilee.rx@gmail.com>
2020-05-18 11:24:10 +0800

    fix(tasks/secure): fix bandit config

--- tasks/secure.py
+++ tasks/secure.py
@@ -3,15 +3,16 @@ from invoke import task
 from tasks.common import COMMON_TARGETS_AS_STR, VENV_PREFIX
 
 
+@task
 def check_package(ctx):
     """Check package security"""
-    ctx.run(f"{VENV_PREFIX} safety")
+    ctx.run(f"{VENV_PREFIX} safety check")
 
 
 @task
 def bandit(ctx):
     """Check common software vulnerabilities (Use it as reference only)"""
-    ctx.run(f"{VENV_PREFIX} bandit -r {COMMON_TARGETS_AS_STR}", pty=True)
+    ctx.run(f"{VENV_PREFIX} bandit -r -iii -lll --ini .bandit", pty=True)
 
 
 @task(pre=[check_package, bandit], default=True)
--- {{cookiecutter.project_slug}}/tasks/secure.py
+++ {{cookiecutter.project_slug}}/tasks/secure.py
@@ -3,6 +3,7 @@ from invoke import task
 from tasks.common import COMMON_TARGETS_AS_STR, VENV_PREFIX
 
 
+@task
 def check_package(ctx):
     """Check package security"""
     {% if cookiecutter.dependency_management_tool == 'pipenv' -%}
@@ -15,7 +16,7 @@ def check_package(ctx):
 @task
 def bandit(ctx):
     """Check common software vulnerabilities (Use it as reference only)"""
-    ctx.run(f"{VENV_PREFIX} bandit -r {COMMON_TARGETS_AS_STR} -iii -lll", pty=True)
+    ctx.run(f"{VENV_PREFIX} bandit -r -iii -lll --ini .bandit", pty=True)
 
 
 @task(pre=[check_package, bandit], default=True)
