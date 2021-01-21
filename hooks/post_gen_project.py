749c8960285c91998a68719a3e595a63fbfed687
Wei Lee <weilee.rx@gmail.com>
2021-01-21 18:42:10 +0800

    feat(github-actions): add pypi-package publishing

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,9 +5,16 @@ def remove_pipfile():
     os.remove("Pipfile")
 
 
+def remove_publish_pypi_github_action():
+    os.remove(".github/workflows/python-publish.yaml")
+
+
 def main():
     if "{{ cookiecutter.dependency_management_tool }}" != "pipenv":
         remove_pipfile()
+
+    if "{{ cookiecutter.build_pypi_package }}" == "n":
+        remove_publish_pypi_github_action()
 
 
 if __name__ == "__main__":
--- {{cookiecutter.project_slug}}/tasks/build.py
+++ {{cookiecutter.project_slug}}/tasks/build.py
@@ -7,5 +7,20 @@ def clean(ctx):
     ctx.run("git clean -Xdf")
 
 
+{% if "{{ cookiecutter.build_pypi_package }}" != "n" -%}
+@task
+def dist(ctx):
+    """Build distribution"""
+    {% if cookiecutter.dependency_management_tool == 'pipenv' -%}
+    ctx.run(f"{VENV_PREFIX} python setup.py sdist bdist_wheel")
+    {%- elif cookiecutter.dependency_management_tool == 'poetry' -%}
+    ctx.run("poetry build")
+    {%- endif %}
+{%- endif %}
+
+
 build_ns = Collection("build")
 build_ns.add_task(clean)
+{% if "{{ cookiecutter.build_pypi_package }}" != "n" -%}
+build_ns.add_task(dist)
+{%- endif %}
