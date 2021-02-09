f13f291bb78bac8671b39a4c7fda1d0420e8ddf3
RainrainWu <s0958334772@gmail.com>
2021-02-09 23:44:16 +0800

    fix(dockerize): patch post hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -10,14 +10,15 @@ def remove_publish_pypi_github_action():
 
 
 def generate_docker_file(yes, python_version):
-    if yes:
-        with open("Dockerfile_general", "r") as rf:
-            docker_file_content = rf.read().format(PYTHON_VERSION=python_version)
-            rf.close()
-        with open("Dockerfile", "a") as wf:
-            wf.write(docker_file_content)
-            wf.close()
-    # os.remove("Dockerfile_general")
+    if not yes:
+        os.remove("Dockerfile")
+        return
+    with open("Dockerfile", "r") as rf:
+        docker_file_content = rf.read().format(PYTHON_VERSION=python_version)
+        rf.close()
+    with open("Dockerfile", "w") as wf:
+        wf.write(docker_file_content)
+        wf.close()
 
 
 def main():
