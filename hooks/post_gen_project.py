82542606e419490a30f3a9e5bb3c9933f617826e
RainrainWu <s0958334772@gmail.com>
2021-02-09 22:36:02 +0800

    feat(dockerize): add general docker file

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -9,12 +9,28 @@ def remove_publish_pypi_github_action():
     os.remove(".github/workflows/python-publish.yaml")
 
 
+def generate_docker_file(yes, python_version):
+    if yes:
+        with open("Dockerfile_general", "r") as rf:
+            docker_file_content = rf.read().format(PYTHON_VERSION=python_version)
+            rf.close()
+        with open("Dockerfile", "w") as wf:
+            wf.write(docker_file_content)
+            wf.close()
+    os.remove("Dockerfile_general")
+
+
 def main():
     if "{{ cookiecutter.dependency_management_tool }}" != "pipenv":
         remove_pipfile()
 
     if "{{ cookiecutter.build_pypi_package }}" == "n":
         remove_publish_pypi_github_action()
+
+    generate_docker_file(
+        "{{ cookiecutter.build_pypi_package }}" != "n",
+        "{{ cookiecutter.python_version }}",
+    )
 
 
 if __name__ == "__main__":
