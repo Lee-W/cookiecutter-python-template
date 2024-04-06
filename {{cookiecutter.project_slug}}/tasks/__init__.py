e86ff1f299c131a2ac6e2b1a121aa39a8774ff23
Wei Lee <weilee.rx@gmail.com>
2024-04-06 18:08:29 +0800

    feat(github-actions): run pre-commit hooks on github action

--- {{cookiecutter.project_slug}}/tasks/__init__.py
+++ {{cookiecutter.project_slug}}/tasks/__init__.py
@@ -1,7 +1,16 @@
 from invoke.collection import Collection
+from invoke.context import Context
 
 from tasks import doc, env, git, secure, style, test
 from tasks.build import build_ns
+from tasks.common import VENV_PREFIX
+
+
+@task
+def run_pre_commit_hooks(context: Context) -> None:
+    """Run pre-commit hooks on all files"""
+    context.run(f"{VENV_PREFIX} pre-commit run --all-files")
+
 
 ns = Collection()
 ns.add_collection(env)  # type: ignore[arg-type]
@@ -11,3 +20,4 @@ ns.add_collection(style)  # type: ignore[arg-type]
 ns.add_collection(build_ns)
 ns.add_collection(secure)  # type: ignore[arg-type]
 ns.add_collection(doc)  # type: ignore[arg-type]
+ns.add_task(run_pre_commit_hooks)  # type: ignore[arg-type]
