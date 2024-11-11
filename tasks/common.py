d677e31024a98c48b4a046f4391e0c56bf760251
Wei Lee <weilee.rx@gmail.com>
2024-11-11 22:00:50 +0800

    build(project-root): replace poetry with uv

--- tasks/common.py
+++ tasks/common.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
-VENV_PREFIX = "poetry run"
+VENV_PREFIX = "uv run"
 _COMMON_TARGETS = ["hooks", "tests"]
 COMMON_TARGETS_AS_STR = " ".join(_COMMON_TARGETS)
--- tasks/env.py
+++ tasks/env.py
@@ -9,13 +9,13 @@ from tasks.common import VENV_PREFIX
 @task
 def clean(ctx: Context) -> None:
     """Remove virtual environment"""
-    ctx.run("poetry env remove 3.12", warn=True)
+    ctx.run("rm -rf .venv", warn=True)
 
 
 @task
 def init(ctx: Context) -> None:
-    """Install production dependencies"""
-    ctx.run("poetry install --without dev")
+    """Install all dependencies"""
+    ctx.run("uv sync")
 
 
 @task
@@ -29,6 +29,6 @@ def setup_pre_commit_hook(ctx: Context) -> None:
 @task(optional=["no-pre-commit"])
 def init_dev(ctx: Context, no_pre_commit=False) -> None:
     """Install development dependencies and setup pre-commit hooks"""
-    ctx.run("poetry install --with dev")
+    init(ctx)
     if not no_pre_commit:
         setup_pre_commit_hook(ctx)
